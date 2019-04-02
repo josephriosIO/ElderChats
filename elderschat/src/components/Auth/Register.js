07742181ff8797543e635dd4d41588a10f82d44c
josephriosio <blankworldwide@gmail.com>
2019-04-01 19:14:28 -0700

    error handling

--- elderschat/src/components/Auth/Register.js
+++ elderschat/src/components/Auth/Register.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import firebase from "../../firebase";
 import {
   Grid,
   Form,
@@ -11,8 +12,88 @@ import {
 } from "semantic-ui-react";
 
 class Register extends Component {
-  state = {};
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirmation: "",
+    errors: [],
+    loading: false
+  };
+
+  isFormValid = () => {
+    let errors = [];
+    let error;
+    if (this.isFormEmpty(this.state)) {
+      error = { message: "Fill in all fields" };
+      this.setState({ errors: errors.concat(error) });
+      return false;
+    } else if (this.isPasswordValid(this.state)) {
+      error = { message: "Password is wrong boiii" };
+      this.setState({ errors: errors.concat(error) });
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  isFormEmpty = ({ username, email, password, passwordConfirmation }) => {
+    return (
+      !username.length ||
+      !email.length ||
+      !password.length ||
+      !passwordConfirmation
+    );
+  };
+
+  isPasswordValid = ({ password, passwordConfirmation }) => {
+    if (password.length < 6 || passwordConfirmation.length < 6) {
+      return false;
+    } else if (password !== passwordConfirmation) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  displayErrors = errors =>
+    errors.map((error, i) => <p key={i}>{error.message}</p>);
+
+  handleChanges = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    if (this.isFormValid()) {
+      this.setState({ errors: [], loading: true });
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(createdUser => {
+          console.log(createdUser);
+          this.setState({ loading: false });
+        })
+        .catch(err => {
+          console.error(err);
+          this.setState({
+            errors: this.state.errors.concat(err),
+            loading: false
+          });
+        });
+    }
+  };
+
   render() {
+    const {
+      username,
+      email,
+      password,
+      passwordConfirmation,
+      loading
+    } = this.state;
     return (
       <Grid textAlign="center" verticalAlign="middle" className="app">
         <Grid.Column style={{ maxWidth: 450 }}>
@@ -20,7 +101,7 @@ class Register extends Component {
             <Icon name="user secret" color="teal" />
             Register for EldersChat
           </Header>
-          <Form size="large">
+          <Form onSubmit={this.handleSubmit} size="large">
             <Segment stacked>
               <Form.Input
                 fluid
@@ -30,6 +111,7 @@ class Register extends Component {
                 placeholder="username"
                 onChange={this.handleChanges}
                 type="text"
+                value={username}
               />
               <Form.Input
                 fluid
@@ -39,6 +121,7 @@ class Register extends Component {
                 placeholder="Email"
                 onChange={this.handleChanges}
                 type="email"
+                value={email}
               />
               <Form.Input
                 fluid
@@ -48,21 +131,35 @@ class Register extends Component {
                 placeholder="Password"
                 onChange={this.handleChanges}
                 type="password"
+                value={password}
               />
               <Form.Input
                 fluid
-                name="password confirmation"
+                name="passwordConfirmation"
                 icon="repeat"
                 iconPosition="left"
                 placeholder="Password Confirmation"
                 onChange={this.handleChanges}
                 type="password"
+                value={passwordConfirmation}
               />
-              <Button color="teal" fluid size="large">
+              <Button
+                disabled={loading}
+                className={`${loading ? "loading" : ""} `}
+                color="teal"
+                fluid
+                size="large"
+              >
                 Submit
               </Button>
             </Segment>
           </Form>
+          {this.state.errors.length > 0 && (
+            <Message error>
+              <h3>Error</h3>
+              {this.displayErrors(this.state.errors)}
+            </Message>
+          )}
           <Message>
             Already a user? <Link to="/login">Login</Link>
           </Message>
