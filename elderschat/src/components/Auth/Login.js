2ca920dae169a965edb339175dbc33022153c019
josephriosio <blankworldwide@gmail.com>
2019-04-01 20:14:10 -0700

    login

--- elderschat/src/components/Auth/Login.js
+++ elderschat/src/components/Auth/Login.js
@@ -1,8 +1,116 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import firebase from "../../firebase";
+import {
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon
+} from "semantic-ui-react";
 
 class Login extends Component {
+  state = {
+    email: "",
+    password: "",
+    errors: [],
+    loading: false
+  };
+  displayErrors = errors =>
+    errors.map((error, i) => <p key={i}>{error.message}</p>);
+
+  handleChanges = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    if (this.isFormValid(this.state)) {
+      this.setState({ errors: [], loading: true });
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.state.email, this.state.password)
+        .then(signedInUser => {
+          console.log(signedInUser);
+        })
+        .catch(err => {
+          console.error(err);
+          this.setState({
+            errors: this.state.errors.concat(err),
+            loading: false
+          });
+        });
+    }
+  };
+
+  isFormValid = ({ email, password }) => email && password;
+
+  handleErrors = (errors, inputName) => {
+    return errors.some(error => error.message.toLowerCase().includes(inputName))
+      ? "error"
+      : "";
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password, loading, errors } = this.state;
+    return (
+      <Grid textAlign="center" verticalAlign="middle" className="app">
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h1" icon color="teal" textAlign="center">
+            <Icon name="code branch" color="teal" />
+            Login To EldersChat
+          </Header>
+          <Form onSubmit={this.handleSubmit} size="large">
+            <Segment stacked>
+              <Form.Input
+                fluid
+                name="email"
+                icon="mail"
+                iconPosition="left"
+                placeholder="Email"
+                onChange={this.handleChanges}
+                type="email"
+                className={this.handleErrors(errors, "email")}
+                value={email}
+              />
+              <Form.Input
+                fluid
+                name="password"
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                onChange={this.handleChanges}
+                type="password"
+                className={this.handleErrors(errors, "password")}
+                value={password}
+              />
+              <Button
+                disabled={loading}
+                className={`${loading ? "loading" : ""} `}
+                color="teal"
+                fluid
+                size="large"
+              >
+                Submit
+              </Button>
+            </Segment>
+          </Form>
+          {this.state.errors.length > 0 && (
+            <Message error>
+              <h3>Error</h3>
+              {this.displayErrors(this.state.errors)}
+            </Message>
+          )}
+          <Message>
+            Don't have an account? <Link to="/register">Register</Link>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    );
   }
 }
 
--- elderschat/src/index.js
+++ elderschat/src/index.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
+import firebase from "./firebase";
 
 // components
 import Login from "./components/Auth/Login";
@@ -10,17 +16,33 @@ import Register from "./components/Auth/Register";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
-const Root = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-    </Switch>
-  </Router>
-);
+class Root extends Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.history.push("/");
+      }
+    });
+  }
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    );
+  }
+}
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(
+  <Router>
+    <RootWithAuth />
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
